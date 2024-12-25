8c870d49042d46ff74cb273213ed66413637e39b
LuxShan Thavarasa <120371059+Luxshan2000@users.noreply.github.com>
2024-12-25 20:50:17 +0530

    Create tailwind.config.js

---
+++ ui/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**/*.html", "./**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#007BFF", // Blue theme
+      },
+    },
+  },
+  plugins: [],
+};
