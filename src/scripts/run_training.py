f378cb906ab37ef8bfd24f32b6a84c6bfa222011
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-13 13:23:59 +0000

    Emota Wav2vec-large-960h

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -289,7 +289,7 @@ if __name__ == "__main__":
 
     # ----------Modify-------------#
     CUR_DATASET = DATASET.EMOTA
-    CUR_BASE_MODEL = BASE_MODEL.WAV2VEC2_BASE.value
+    CUR_BASE_MODEL = BASE_MODEL.WAV2VEC2_LARGE_960H.value
 
     logger.info(
         f"Start finetuning {CUR_BASE_MODEL} with {CUR_DATASET.value.name}"
