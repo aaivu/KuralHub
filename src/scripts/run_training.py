b83b0f55dfb108bffca0bdfabb2d7d18b5e74021
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-13 17:43:37 +0000

    EmoTa wavLM-large

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -289,7 +289,7 @@ if __name__ == "__main__":
 
     # ----------Modify-------------#
     CUR_DATASET = DATASET.EMOTA
-    CUR_BASE_MODEL = BASE_MODEL.WAVLM_BASE_PLUS.value
+    CUR_BASE_MODEL = BASE_MODEL.WAVLM_LARGE.value
 
     logger.info(
         f"Start finetuning {CUR_BASE_MODEL} with {CUR_DATASET.value.name}"
