6bc1da9652b6c33be55e4e9fbeb25b082988f88a
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-16 12:42:40 +0000

    Emota Whisper

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -289,7 +289,7 @@ if __name__ == "__main__":
 
     # ----------Modify-------------#
     CUR_DATASET = DATASET.EMOTA
-    CUR_BASE_MODEL = BASE_MODEL.HUBERT_BASE.value
+    CUR_BASE_MODEL = BASE_MODEL.HUBERT_LARGE_LS960.value
 
     logger.info(
         f"Start finetuning {CUR_BASE_MODEL} with {CUR_DATASET.value.name}"
