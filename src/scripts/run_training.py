43c6433b73c5949fe8666f6952452ef9831dcb2f
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-16 16:01:08 +0000

    Telugu-Dataset hubert-large

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -289,7 +289,7 @@ if __name__ == "__main__":
 
     # ----------Modify-------------#
     CUR_DATASET = DATASET.TELUGU_DATASET
-    CUR_BASE_MODEL = BASE_MODEL.WAV2VEC2_LARGE_LV60.value
+    CUR_BASE_MODEL = BASE_MODEL.XLS_R_1B.value
     # ----------End-------------#
 
     logger.info(
