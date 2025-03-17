9c47d31ccdc4436d65c03d9f69308cc33437b8e3
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-17 11:03:23 +0000

    cuda:device_id added

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -285,7 +285,17 @@ def train(
 
 
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        print(f"Total CUDA devices: {torch.cuda.device_count()}")
+        for i in range(torch.cuda.device_count()):
+            print(f"Device {i}: {torch.cuda.get_device_name(i)}")
+    else:
+        print("CUDA is not available.")
+
+    device_id = 0
+    device = torch.device(
+        f"cuda:{device_id}" if torch.cuda.is_available() else "cpu"
+    )
 
     # ----------Modify-------------#
     CUR_DATASET = DATASET.KANNADA_DATASET
