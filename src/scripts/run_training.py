b6e21c4091fb0325cf833e2dd6de50be14f39772
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-18 16:15:54 +0000

    RAVDESS added

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -292,14 +292,14 @@ if __name__ == "__main__":
     else:
         print("CUDA is not available.")
 
-    device_id = 0
+    device_id = 1
     device = torch.device(
         f"cuda:{device_id}" if torch.cuda.is_available() else "cpu"
     )
 
     # ----------Modify-------------#
-    CUR_DATASET = DATASET.IESC
-    CUR_BASE_MODEL = BASE_MODEL.OPENAI_WHISPER_LARGE.value
+    CUR_DATASET = DATASET.ASED
+    CUR_BASE_MODEL = BASE_MODEL.OPENAI_WHISPER_SMALL.value
     # ----------End-------------#
 
     logger.info(
