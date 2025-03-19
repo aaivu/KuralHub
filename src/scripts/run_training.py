5c1e761409c5393a86c2233b1a16f2a461ce84e6
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-19 19:57:00 +0000

    ESD

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -298,8 +298,8 @@ if __name__ == "__main__":
     )
 
     # ----------Modify-------------#
-    CUR_DATASET = DATASET.MESD
-    CUR_BASE_MODEL = BASE_MODEL.WAV2VEC2_BASE.value
+    CUR_DATASET = DATASET.URDU_DATASET
+    CUR_BASE_MODEL = BASE_MODEL.XLS_R_300M.value
     # ----------End-------------#
 
     logger.info(
