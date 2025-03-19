bf7a67be137b8089145bd2ee3824e45a37366f52
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-19 10:54:18 +0000

    CaFE wavLM large

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -292,13 +292,13 @@ if __name__ == "__main__":
     else:
         print("CUDA is not available.")
 
-    device_id = 0
+    device_id = 1
     device = torch.device(
         f"cuda:{device_id}" if torch.cuda.is_available() else "cpu"
     )
 
     # ----------Modify-------------#
-    CUR_DATASET = DATASET.CAFE
+    CUR_DATASET = DATASET.ESD_CHINESE
     CUR_BASE_MODEL = BASE_MODEL.XLS_R_1B.value
     # ----------End-------------#
 
