73505bdd36f48146af6b3a05f63c45b43333037b
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-24 10:45:45 +0000

    KESDy18 added wav2vec large

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -292,14 +292,14 @@ if __name__ == "__main__":
     else:
         print("CUDA is not available.")
 
-    device_id = 1
+    device_id = 0
     device = torch.device(
         f"cuda:{device_id}" if torch.cuda.is_available() else "cpu"
     )
 
-    # ----------Modify-------------#
-    CUR_DATASET = DATASET.KESDy18
-    CUR_BASE_MODEL = BASE_MODEL.XLS_R_1B.value
+    # ----------Modify-------------#MSER20
+    CUR_DATASET = DATASET.SITBOSED
+    CUR_BASE_MODEL = BASE_MODEL.WAV2VEC2_BASE.value
     # ----------End-------------#
 
     logger.info(
