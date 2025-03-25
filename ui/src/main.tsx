c41b9ce96baf919e9763dd75eb3fc37a147f14d1
Luxshan <luxshanlux2000@gmail.com>
2025-03-25 08:01:23 +0530

    UI initial refactor

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './theme.css'
-import 'highlight.js/styles/github.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
