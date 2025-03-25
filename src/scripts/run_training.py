5256470310ab9ab70963187224c0e6e2558fb278
Luxshan2000 <luxshanlux2000@gmail.com>
2025-03-25 13:58:42 +0000

    emoUERJ and TurEV-DB added

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -299,7 +299,7 @@ if __name__ == "__main__":
 
     # ----------Modify-------------#MSER20
     CUR_DATASET = DATASET.EMOUERJ
-    CUR_BASE_MODEL = BASE_MODEL.WAV2VEC2_BASE.value
+    CUR_BASE_MODEL = BASE_MODEL.HUBERT_BASE.value
     # ----------End-------------#
 
     logger.info(
