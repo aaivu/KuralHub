03e8c763647e2b326035e75b1a7a783ca0a3a444
Luxshan2000 <luxshanlux2000@gmail.com>
2025-04-09 15:10:25 +0000

    UrduSER and TurEVDB and Emozionalmente missing added

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -299,7 +299,7 @@ if __name__ == "__main__":
 
     # ----------Modify-------------#MSER20
     CUR_DATASET = DATASET.URDUSER
-    CUR_BASE_MODEL = BASE_MODEL.XLS_R_300M.value
+    CUR_BASE_MODEL = BASE_MODEL.WAV2VEC2_LARGE_LV60.value
     # ----------End-------------#
 
     logger.info(
