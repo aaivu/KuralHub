354b462e678a15ae7f0fe1de2cc585da96f4b578
Luxshan2000 <luxshanlux2000@gmail.com>
2025-04-11 18:54:56 +0000

    Hungarian added

--- src/scripts/run_training.py
+++ src/scripts/run_training.py
@@ -294,14 +294,14 @@ if __name__ == "__main__":
     else:
         print("CUDA is not available.")
 
-    device_id = 0
+    device_id = 1
     device = torch.device(
         f"cuda:{device_id}" if torch.cuda.is_available() else "cpu"
     )
 
-    # ----------Modify-------------#MSER20
-    CUR_DATASET = DATASET.AFRIKAANSEMOTIONALSPEECHCORPUS
-    CUR_BASE_MODEL = BASE_MODEL.WAV2VEC2_LARGE_LV60.value
+    # ----------Modify-------------#
+    CUR_DATASET = DATASET.HUNGARIANEMOTIONALSPEECHCORPUS
+    CUR_BASE_MODEL = BASE_MODEL.XLS_R_1B.value
     # ----------End-------------#
 
     logger.info(
