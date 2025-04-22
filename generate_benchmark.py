58500f6c760c7c92f8dd99f74ef1ba46f51f2180
Luxshan2000 <luxshanlux2000@gmail.com>
2025-04-22 00:08:17 +0000

    train_val_test_logs moved

--- generate_benchmark.py
+++ generate_benchmark.py
@@ -79,7 +79,7 @@ def generate_benchmark(directory, output_md, output_json):
     
     print(f"Markdown file '{output_md}' and JSON file '{output_json}' generated successfully!")
 
-directory = "./ui/public/train_val_test_logs"
+directory = "./ui/train_val_test_logs"
 output_md = "benchmark.md"
-output_json = "./ui/src/benchmark.json"
+output_json = "./ui/benchmark.json"
 generate_benchmark(directory, output_md, output_json)
